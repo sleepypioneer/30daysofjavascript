1c23ff5d9ac9cbc1cc76fdaec2ba1da5ac1be56d
sleepypioneer <jessica0greene@gmail.com>
2017-11-29 22:42:31 +0100

    additional styling and functions for navigation on index.html
    Signed-off-by: sleepypioneer <jessica0greene@gmail.com>

--- app/scripts/indexScripts.js
+++ app/scripts/indexScripts.js
@@ -1,11 +1,42 @@
-window.addEventListener('load', resizePageOne);
-window.addEventListener('resize', resizePageOne);
+/*************** Global Variables ***************/
+const p = document.getElementById('projects');
+const home = document.querySelector('#returnHome');
+const pageOne = document.querySelector('#pageOne');
+const container = document.querySelector('.container');
 
 
 
+/*************** Event Listeners ***************/
+window.addEventListener('load', init);
+p.addEventListener('click', scrollToProjects);
+home.addEventListener('click', returnHome);
+
+/*************** Functions **************
 function resizePageOne() {
-    console.log(window.innerHeight);   
-    pageOne = document.querySelector('#pageOne');
+    
     pageOne.style.height = window.innerHeight +"px";
-    console.log(pageOne.style.height);
+}
+*/
+
+function scrollToProjects() {
+    pageOne.style.display = 'none';
+    home.style.visibility = 'visible';
+    container.style.top = "0";
+
+}
+
+function returnHome() {
+    pageOne.style.display = 'flex';
+    home.style.visibility = 'hidden';
+    container.style.top = "100vh";
+}
+
+
+
+
+
+
+
+function init() {
+    
 }
